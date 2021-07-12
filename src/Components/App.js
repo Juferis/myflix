fa948b760258482b99ac5626e060e4c9c5a9ad11
Juferis <66511872+Juferis@users.noreply.github.com>
2021-07-12 18:12:23 +0900

    create routers

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+import React, { Component } from "react";
+import Router from "Components/Router";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router />
+      </>
+    );
+  }
+  // <></>는 Fragments라고 하며 원하는 만큼 컴포넌트를 return활 수 있게 해준다.
+}
+
+export default App;
