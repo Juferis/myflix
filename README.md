d574d74f2ebc149e6c1a63eebcc5b57a26e97582
Juferis <66511872+Juferis@users.noreply.github.com>
2021-07-14 17:27:23 +0900

    React Router pt.2

--- README.md
+++ README.md
@@ -12,3 +12,9 @@
 ## 1.0 사용한 도구
 
 - React Router : 간단한 컴포넌트 묶음, React에게 시작 라우터 지정가능
+
+## 2.0 새로 학습한 내용
+
+> 사진의 주소에 #이 있는것은 Hash Router(앱처럼 보이게) 방식이다.  
+> #이 없는 것은 Browser Router이며 HTML history API(웹처럼 보이게)라는 것을 사용한다.  
+> ![img](img/new_01.jpg)
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 // eslint-disable-next-line
 export default () => (
+  // Compostion은 두 개 이상의 Route를 랜더링하는 방식(동시에)
   <Router>
     <>
       <Route path="/" exact component={Home} />
       <Route path="/search" exact component={Search} />
       <Route path="/tv" exact component={TV} />
+      <Redirect from="*" to="/" />
     </>
   </Router>
 );
