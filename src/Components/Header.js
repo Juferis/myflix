f87dc61d46a3ea057054309688da298b7457c97a
Juferis <66511872+Juferis@users.noreply.github.com>
2021-07-22 21:41:36 +0900

    Lccation Aware Header with withRouter

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 // 이 방식을 이용하면 클래스명이 겹치지 않는다.
@@ -12,7 +12,6 @@ const Header = styled.header`
   height: 50px;
   display: flex;
   align-items: center;
-  padding: 0px 10px;
   background-color: rgba(20, 20, 20, 0.8);
   z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(20, 20, 20, 0.8);
@@ -23,31 +22,38 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-  width: 50px;
+  width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 5px solid
+    ${(props) => (props.current ? "#E50914" : "transparent")};
+  transition: border-bottom 0.2s ease-in-out;
 `;
 
 const SLink = styled(Link)`
   height: 50px;
-  display: block;
+  display: flex;
   align-items: center;
   justify-content: center;
 `;
 
 // eslint-disable-next-line
-export default () => (
+const Headers = (
+  { location: { pathname } } // loaction: { pathname } props를 console.log하면 나오는 오브젝트의 값이다.
+) => (
   <Header>
     <List>
-      <Item>
-        <SLink to="/">Home</SLink>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Movies</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/tv"}>
         <SLink to="/tv">TV</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/search"}>
         <SLink to="/search">Search</SLink>
       </Item>
     </List>
   </Header>
 );
+
+export default withRouter(Headers);
