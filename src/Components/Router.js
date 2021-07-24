eff776ee032d0f974d70f334b6a4f4fd737a283c
Juferis <66511872+Juferis@users.noreply.github.com>
2021-07-24 20:52:28 +0900

    Setting Route for Detail

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
+import Detail from "Routes/Detail";
 import Header from "Components/Header"; // "Components/Header/Header" 주소가 이렇게 되지 않는 이유는 Headr 폴더 내에 index.js가 있기 때문
 // eslint-disable-next-line
 export default () => (
@@ -13,6 +14,8 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/search" exact component={Search} />
       <Route path="/tv" exact component={TV} />
+      <Route path="/movie/:id" component={Detail} />
+      <Route path="/tv/:id" component={Detail} />
       <Redirect from="*" to="/" />
     </>
   </Router>
